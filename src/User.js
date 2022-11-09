83c34af944549ab061423a361971aac245ce48fb
digiguru <digiguru@gmail.com>
2022-11-09 22:40:46 +0000

    Initial Commit

--- src/User.js
+++ src/User.js
@@ -1,18 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-import Messages from './Messages';
-import MessageInput from './MessageInput';
 
-import './App.css';
 
-function App() {
-  const [socket, setSocket] = useState(null);
-
-  useEffect(() => {
-    const newSocket = io(`http://${window.location.hostname}:3000`);
-    setSocket(newSocket);
-    return () => newSocket.close();
-  }, [setSocket]);
+function User({socket}) {
 
   return (
     <div className="App">
@@ -25,7 +15,7 @@ function App() {
           <MessageInput socket={socket} />
         </div>
       ) : (
-        <div>Not Connected</div>
+        <div>App is offline</div>
       )}
     </div>
   );
