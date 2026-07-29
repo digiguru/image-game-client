468fc77d5bb9265705e61af39a196b304163c39d
digiguru <digiguru@gmail.com>
2026-07-29 16:13:20 +0100

    refactor: rename Users component to jsx

---
+++ src/Users.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Users.css';
+
+function Users({ users }) {
+  return (
+    <div className="user-list">
+      {[...Object.values(users)]
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((user) => (
+          <div
+            key={user.userID}
+            className="user-container"
+            title={`Added at ${new Date(user.time).toLocaleTimeString()}`}
+          >
+            <span className="user">{user.name}</span>
+          </div>
+        ))
+      }
+    </div>
+  );
+}
+
+export default Users;
