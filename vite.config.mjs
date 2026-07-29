672e54ea0c50183d99f7ece51fa4b0927bb9eaef
digiguru <digiguru@gmail.com>
2026-07-29 16:11:12 +0100

    fix: use standard Vite React configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,23 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react({
-      include: /\.[jt]sx?$/
-    })
-  ],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: []
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx'
-      }
-    }
-  },
+  plugins: [react()],
   server: {
     host: true,
     port: 3000
