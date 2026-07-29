4d8464e413a249c8ce63d453e835483e8384929c
digiguru <digiguru@gmail.com>
2026-07-29 16:16:16 +0100

    refactor: remove old Image js file

--- src/Image.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Image = ({ image, alt, onClick, clickable }) => {
-  const hoverable = (typeof onClick === 'function');
-  const style = hoverable ? clickable ? {cursor: "pointer"} : {cursor: "not-allowed"} : {};
-  const handleClick = clickable ? onClick : ()=>{};
-  return (image && <img alt={alt} style={style} src={image.startsWith("http") ? image : "data:image/jpeg;base64, " + image} onClick={handleClick} />);
-};
