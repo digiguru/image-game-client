0a371fd6e0f1a08d81042e3187ed3b8e71cd64ce
digiguru <digiguru@gmail.com>
2026-07-29 15:12:45 +0100

    Sync image accessibility changes from main

--- src/Image.js
+++ src/Image.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const Image = ({ image, onClick, clickable }) => {
+export const Image = ({ image, alt, onClick, clickable }) => {
   const hoverable = (typeof onClick === 'function');
   const style = hoverable ? clickable ? {cursor: "pointer"} : {cursor: "not-allowed"} : {};
   const handleClick = clickable ? onClick : ()=>{};
-  return (image && <img style={style} src={image.startsWith("http") ? image : "data:image/jpeg;base64, " + image} onClick={handleClick} />);
+  return (image && <img alt={alt} style={style} src={image.startsWith("http") ? image : "data:image/jpeg;base64, " + image} onClick={handleClick} />);
 };
