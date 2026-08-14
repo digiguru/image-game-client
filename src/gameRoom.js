e80fb9fdbe2186331577939c560ef341306c9027
digiguru <digiguru@gmail.com>
2026-08-14 14:05:45 +0100

    Use explicit browser globals

--- src/gameRoom.js
+++ src/gameRoom.js
@@ -6,13 +6,13 @@ export function normaliseRoomID(value) {
   return /^[a-zA-Z0-9_-]{1,40}$/.test(roomID) ? roomID : DEFAULT_ROOM_ID;
 }
 
-export function getRoomID(search = window.location.search) {
-  const params = new URLSearchParams(search);
+export function getRoomID(search = globalThis.window.location.search) {
+  const params = new globalThis.URLSearchParams(search);
   return normaliseRoomID(params.get('room'));
 }
 
-export function getPlayerShareUrl(roomID, location = window.location) {
-  const url = new URL(location.origin);
+export function getPlayerShareUrl(roomID, location = globalThis.window.location) {
+  const url = new globalThis.URL(location.origin);
   url.searchParams.set('room', normaliseRoomID(roomID));
   return url.toString();
 }
